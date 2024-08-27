d670988e0d1ea61050119c7a182691374a8a0c08
jme <jme.co.jp@gmail.com>
2024-08-27 17:29:48 +0900

    migrate

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,16 @@ mod api;
 use api::hello::hello;
 
 use axum::{routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() {
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect("postgres://postgres:password@localhost:5432")
+        .await
+        .unwrap();
+
     let app = Router::new().route("/hello", get(hello));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
